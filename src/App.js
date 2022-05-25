e09f8959f217d9076a4c9d5c0abea9cd41a61589
Igorplisko <igorplisko95@gmail.com>
2022-05-25 18:50:04 +0300

    Today is May 25, 2022 and it's the 91 day of the war!

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Today is May 25, 2022 and it's the 91 day of the war!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+//! Today is May 25, 2022 and it's the 91 day of the war!

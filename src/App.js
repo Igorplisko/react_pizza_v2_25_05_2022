94761c436922b530fb0abbfbac78aef339b7ad1b
Igorplisko <igorplisko95@gmail.com>
2022-05-26 17:25:05 +0300

    inn

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ function App() {
   return (<
     < div className="App" > < h1 > Today is May 25, 2022 and it 's the 91 day of the war!</h1>
 
+      < div className="App" > < h1 > Today is May 25, 2022 and it 's the 91 day of the war!</h1>
 
 
 
 
 
-      <
+        <
     /div>
-      );
+        );
 }
 
-      export default App;
+        export default App;
 
 
 

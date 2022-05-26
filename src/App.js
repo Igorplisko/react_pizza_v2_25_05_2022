a788f1c75ebb873f78631b5826d7e69f6930e9a2
Igorplisko <igorplisko95@gmail.com>
2022-05-26 17:09:40 +0300

    innn=it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Today is May 25, 2022 and it's the 91 day of the war!</h1>
-    </div>
-  );
+  return (<
+    < div className="App" > < h1 > Today is May 25, 2022 and it 's the 91 day of the war!</h1>
+
+
+
+
+
+
+      <
+    /div>
+      );
 }
 
-export default App;
+      export default App;
 
 
 

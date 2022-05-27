7179914959860b30e79a53207c78fa12054d46a5
Igorplisko <igorplisko95@gmail.com>
2022-05-27 23:09:12 +0300

    INNN

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
 
-function App() {
-           
+import React from 'react';
 
-         let fsg =    25 
-
-  return (
-                < div className="App" >
-      < h1 > Today is May 25, 2022 and it 's the 91 day of the war!</h1>
-
-      < div className="App" >
-        < h1 > Today is May 25, 2022 and it 's the 91 day of the war!</h1>
-
-        vbb
-        dddffsfvsvvs
-
-      </div>
-      );
+export default function App() {
+  return <div>App</div>;
 }
 
+function App() {
+  return (
+    <div className="App">
+      <h1> Today is May 25, 2022 and it 's the 91 day of the war!</h1>
 
+      <div className="App">
+        <h1> Today is May 25, 2022 and it 's the 91 day of the war!</h1>
+      </div>
+    </div>
+  );
+}
 
 //?</div>
 //! Today is May 25, 2022 and it's the 91 day of the war!

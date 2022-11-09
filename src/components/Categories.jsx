860763aa27ca4a2a5d45acf74b85812e96e85486
Igorplisko <igorplisko95@gmail.com>
2022-11-09 23:29:04 +0200

    commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,28 @@
+import React from 'react';
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(3);
+  const onClickCategory = () => {
+    setActiveIndex(3);
+  };
   return (
     <div className="categories">
       <ul>
         <li className="active">Все</li>
         <li>Meat</li>
         <li>Vegetarian</li>
-        <li>Grensive</li>
+        <li
+          onClick={() => onClickCategory}
+          className={activeIndex === 3 ? 'active' : ''}
+        >
+          Grensive
+        </li>
         <li>Sharp</li>
         <li>Closed</li>
       </ul>
     </div>
 
-    // start 05.09.2022
+    // start 11.11.2022
   );
 }
 
